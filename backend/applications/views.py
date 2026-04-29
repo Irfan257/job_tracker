18f93f9ba1259aa54dbb2eb57911b9774e0e5b95
irfan257 <mdirfan99967@gmail.com>
2026-04-29 11:47:04 +0530

    add REST API and JWT AUTHENTICATION

--- backend/applications/views.py
+++ backend/applications/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import JobApplication
+from .serializers import JobApplicationSerializer
 
-# Create your views here.
+class JobApplicationViewSet(viewsets.ModelViewSet):
+    serializer_class = JobApplicationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return JobApplication.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
